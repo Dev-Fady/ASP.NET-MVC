9d5f04198fb7ba325449209cb62a908481aa1b12
Dev-Fady <fadymobiledev@gmail.com>
2025-09-16 16:16:37 +0300

    defult file in MVC

---
+++ Q3DotNetAssiut/Q3DotNetAssiut/Filters/HandelErrorAttribute.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Q3DotNetAssiut.Filters
+{
+    public class HandelErrorAttribute : Attribute,IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            //ContentResult contentResult = new ContentResult();
+            //contentResult.Content = "Some Exception Throw";
+            ViewResult viewResult = new ViewResult();
+            viewResult.ViewName = "Error";
+            context.Result = viewResult;
+        }
+    }
+}
