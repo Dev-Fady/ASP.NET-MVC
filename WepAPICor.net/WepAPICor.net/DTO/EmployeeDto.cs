cf94149827114a2f0288869f3cfc887eb017e1de
Dev-Fady <fadymobiledev@gmail.com>
2025-09-20 16:54:32 +0300

    general Response , Employee DTO

---
+++ WepAPICor.net/WepAPICor.net/DTO/EmployeeDto.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WepAPICor.net.DTO
+{
+    public class EmployeeDto
+    {
+        public int Id { get; set; }  // ممكن تخليه بس للـ Update
+
+        [Required(ErrorMessage = "Name is required")]
+        [MaxLength(100, ErrorMessage = "Name must be less than 100 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Salary is required")]
+        [Range(1000, 100000, ErrorMessage = "Salary must be between 1000 and 100000")]
+        public int Salary { get; set; }
+
+        [Required(ErrorMessage = "Job Title is required")]
+        [MaxLength(50, ErrorMessage = "Job Title must be less than 50 characters")]
+        public string JopTitle { get; set; }
+
+        [Url(ErrorMessage = "ImageUrl must be a valid URL")]
+        public string? ImageUrl { get; set; }  // optional
+
+        [Required(ErrorMessage = "DepartmentId is required")]
+        public int DepartmentId { get; set; }
+    }
+}
---
+++ WepAPICor.net/WepAPICor.net/DTO/GeneralResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace WepAPICor.net.DTO
+{
+    public class GeneralResponse
+    {
+        //public int StatsCode { get; set; }
+        //public T Data { get; set; }
+
+        public bool IsSuccess { get; set; }
+        public dynamic Data { get; set; }
+    }
+}
